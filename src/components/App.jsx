4d276b9bd47326fa1661baaeb7dc2ec33841159e
Andrey5353 <andreyae0117ab@gmail.com>
2022-10-24 19:45:52 +0300

    hw-react-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
-import{FriendList}from "./FriendList/FriendList"
+import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
 import UserData from "data/user.json";
 import StatisticsData from "data/data.json";
-import FriendsData from "data/friends.json"
+import FriendsData from "data/friends.json";
+import TransactionsDate from "data/transactions.json";
 
 export const App = () => {
   return (
@@ -12,6 +14,7 @@ export const App = () => {
       <Profile props={UserData} />
       <Statistics title="Upload stats" props={StatisticsData} />
       <FriendList props={FriendsData} />
+      <TransactionHistory props={TransactionsDate} />
     </main>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import css from "components/TransactionHistory/TransactionHistory.module.css";
+
+export const TransactionHistory = ({ props }) => {
+    return (
+        <section className={css.transactionSection}>
+            <h1 className={css.sectionTitle}>TRANSACTION HISTORY</h1>
+            <table className={css.transactionHistory}>
+                <thead className={css.transactionTitle}>
+                    <tr>
+                        <th>Type</th>
+                        <th>Amount</th>
+                        <th>Currency</th>
+                    </tr>
+                </thead>
+                {props.map(({ id, type, amount, currency }) => (
+                    <tbody className={css.transactionData} key={id}>
+                        <tr>
+                            <td className={css.transactionType}>{type}</td>
+                            <td>{amount}</td>
+                            <td>{currency}</td>
+                        </tr>
+                    </tbody>
+                ))
+                }
+            </table>
+        </section>
+     
+    )
+};
+
+
+TransactionHistory.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.exact({
+            id: PropTypes.string.isRequired,
+            type: PropTypes.string.isRequired,
+            amount: PropTypes.string.isRequired,
+            currency: PropTypes.string.isRequired,
+        }))
+}
